7e9fe37cd0cf00d95d1870a1dd34c31317bea492
Vivek Sekhar <vivek@viveksekhar.ca>
2019-11-25 07:31:05 -0500

    Just 'switch', it's cleaner.

--- math.go
+++ math.go
@@ -30,23 +30,23 @@ func intPow(x, y int) int {
 
 // intLog returns the largest integer smaller than or equal to log_b(n).
 func intLog(n, b int) int {
-	if n < 1 || b < 1 {
-		panic("n and b must be greater than zero")
-	}
-	if b == 2 {
+	switch {
+	case n < 1:
+		panic("n must be greater than 0")
+	case b < 1:
+		panic("b must be greater than zero")
+	case b == 2:
 		return bits.Len(uint(n)) - 1
-	}
-	fn, fb := float64(n), float64(b)
-	if b&(b-1) == 0 {
+	case b == 10:
+		return int(math.Log10(float64(n)))
+	case b&(b-1) == 0:
 		if n&(n-1) != 0 {
 			return bits.Len(uint(n)) / bits.Len(uint(b))
 		}
-		return int(math.Log2(fn)/float64(bits.Len(uint(b)))) + 1
+		return int(math.Log2(float64(n))/float64(bits.Len(uint(b)))) + 1
+	default:
+		return int(math.Log(float64(n)) / math.Log(float64(b)))
 	}
-	if b == 10 {
-		return int(math.Log10(fn))
-	}
-	return int(math.Log(fn) / math.Log(fb))
 }
 
 func firstChild(pos, h, b int) int {
@@ -69,14 +69,13 @@ func parent(pos, h, b int) int {
 //
 // Source: https://github.com/mimblewimble/grin/blob/78220febeda94595159ece675e77e26986a3c11d/core/src/core/pmmr/pmmr.rs#L402
 func peaks(n, b int) []int {
-	if n < 1 {
-		panic("size of MMR must be positive")
-	}
-	if b < 2 {
+	switch {
+	case n < 1:
+		panic("size of MMR must be greater than 0")
+	case b < 2:
 		panic("branching factor must be at least 2")
-	}
-	var peaks []int
-	if b == 2 {
+	case b == 2:
+		var peaks []int
 		p := 0 // partition (advances as we bag peaks)
 		for n-p > 0 {
 			nextPeak := intPow(b, (intLog(n-p+1, b))) - 1
@@ -84,29 +83,27 @@ func peaks(n, b int) []int {
 			p += nextPeak
 		}
 		return peaks
+	default:
+		// Possibly helpful:
+		// https://ece.uwaterloo.ca/~dwharder/aads/Lecture_materials/5.04.N-ary_trees.pdf
+		panic("branching factors other than 2 are not implemented")
 	}
-	// Possibly helpful:
-	// https://ece.uwaterloo.ca/~dwharder/aads/Lecture_materials/5.04.N-ary_trees.pdf
-	panic("branching factors other than 2 are not implemented")
 }
 
 // height returns the height (counting from 0) of the node at index n in the MMR with
 // branching factor b.
 func height(n, b int) int {
-	if n < 0 {
+	switch {
+	case n < 0:
 		panic("index cannot be negative")
-	}
-	if b < 2 {
+	case b < 2:
 		panic("branching factor must be at least 2")
-	}
-	if n == 0 {
+	case n == 0:
 		return 0
-	}
-	if n < b {
+	case n < b:
 		return 0
-	}
-	var un = uint(n)
-	if b == 2 {
+	case b == 2:
+		var un = uint(n)
 		// bit-shifting fast-path
 		// peakSize := uint(intPow(b, intLog(n, b)+1)) - 1
 		// optimised for b=2
@@ -119,6 +116,7 @@ func height(n, b int) int {
 			peakSize >>= 1
 		}
 		return int(un)
+	default:
+		panic("branching factors other than 2 are not implemented")
 	}
-	panic("branching factors other than 2 are not implemented")
 }
